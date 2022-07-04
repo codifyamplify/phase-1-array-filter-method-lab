77cd0853c5da9cce708b43bad3ac853a36a378b6
David Whitney <codifyamplify@gmail.com>
2022-07-04 17:25:04 -0400

    commit tree c96b289288940e1cd6dbe3b3fa9e93152017eb51

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+// Code your solution here 
+function findMatching(collection, string){
+    return collection.filter((possibleMatch) => possibleMatch.toLowerCase() === string.toLowerCase())
+}
+console.log("~~~~~~~~~~")
+function fuzzyMatch(collection, testString){
+    return collection.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+}
+console.log("~~~~~~~~~~")
+function matchName(collection, query){
+    return collection.filter((entry) => entry.name === query)
+}
